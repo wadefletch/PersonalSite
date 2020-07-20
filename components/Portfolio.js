76edc7e4eccb044a32146f35b013ee082320c3af
Wade Fletcher <wbfletch@gmail.com>
2020-07-20 17:46:08 -0400

    add footnote

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -169,13 +169,19 @@ export class Portfolio extends Component {
                     <p>I am comfortable programming in Python and Java, as well as the typical web technologies (HTML+JS+CSS). Lately I've been vibing with Django and Tailwind, as well as learning React.</p>
                     <p>I won an <a href="https://theemmys.tv/nspa-2019-winners-announced/">Emmy</a> (well, a student Emmy) in 2019. <a href="https://en.wikipedia.org/wiki/Bill_Nye">Bill Nye</a> presented our category.</p>
                     <p>I'm an <a href="https://en.wikipedia.org/wiki/Eagle_Scout_(Boy_Scouts_of_America)">Eagle Scout</a> in <a href="https://en.wikipedia.org/wiki/Boy_Scouts_of_America">BSA</a> Troop 202 and a <a href="https://en.wikipedia.org/wiki/Order_of_the_Arrow_honors_and_awards#Vigil_Honor">Vigil Honor</a> member of the <a href="https://en.wikipedia.org/wiki/Order_of_the_Arrow">Order of the Arrow</a>, Muscogee Lodge #221.</p>
-                    <p>I've some familiarity with hackathons&mdash;participating in more of them in the next year is one of my goals.</p>
+                    <p>I've some familiarity with hackathons&mdash;participating in more of them in the next year is one of my goals.<sup>4</sup></p>
                     <MinorSection title="hackathons and competitions">
                         <ul>
                             <li><strong>1st place @ Intel-OA Hackathon</strong>&nbsp;(Fall 2017): won first place in the Communications division in&nbsp;the inaugural <a href="http://event.oa-bsa.org/events/hackathon2017/">Intel-OA Hackathon</a> with <a href="https://github.com/wadefletch/NOACTS">NOACTS</a>.</li>
                             <li><strong>1st place @ CodeDay Columbia</strong> (Spring 2016): won first place in the Web division at <a href="https://www.codeday.org/columbia">CodeDay Columbia</a> with <a href="https://github.com/wadefletch/theforest">TheForest</a>.</li>
                         </ul>
                     </MinorSection>
+                    <div className="text-xs text-gray-600">
+                        <div>
+                            <sup style={{marginRight: "2px"}}>4</sup>
+                            I imagine COVID-19 will make this difficult.
+                        </div>
+                    </div>
                 </MajorSection>
             </Section>
         );
