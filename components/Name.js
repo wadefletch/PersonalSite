6bce988c0db2fab168886f72f56a1e01d36f2e29
Wade Fletcher <wbfletch@gmail.com>
2020-11-26 21:01:46 -0500

    change color of tagline divider

--- components/Name.js
+++ components/Name.js
@@ -5,12 +5,12 @@ export class Name extends Component {
     render() {
         return (
             <Section>
-                <div className="mt-24 mb-16 ml-0 md:ml-4 md:pl-36">
+                <div className="mt-24 mb-16 ml-0 md:ml-4 md:pl-20 xl:pl-32">
                     <div className='font-extrabold leading-tight text-5xl w-full'>Wade Fletcher</div>
                     <div className='ml-2 text-gray-400 font-medium text-xl w-full'>
                         Building <a href='//undercover.vc' className='text-gray-300 transition hover:text-red-600 font-semibold no-underline'>@UndercoverVC</a>
 
-                        <br className='block sm:hidden'/><span className='text-gray-400'> \\ </span>
+                        <br className='block sm:hidden'/><span className='text-gray-600'> \\ </span>
 
                         CS and Finance at <a href='//indiana.edu' className='text-gray-300 transition hover:text-red-600 font-semibold no-underline'>@Indiana</a>
                     </div>
