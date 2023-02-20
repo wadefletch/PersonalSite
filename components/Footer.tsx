5390bfc9e20d40e7475f95eafb7035a4a53b4cb6
Wade Fletcher <wbfletch@gmail.com>
2023-02-20 14:50:39 -0500

    improve fallback for missing commit hash

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,17 @@
-const Footer = () => (
-  <footer className="text-sm text-gray-500 grid place-content-center max-w-2xl mx-auto p-5">
-    CC BY 4.0 &#x2022; 2023 &#x2022;{' '}
-    {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.substring(0, 7)}
-  </footer>
-);
+const Footer = () => {
+  const sha = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.substring(0, 7);
+
+  return (
+    <footer className="text-sm text-gray-500 grid place-content-center max-w-2xl mx-auto p-5">
+      CC BY 4.0 &#x2022; 2023
+      {sha && (
+        <>
+          <span>&#x2022;</span>
+          {sha}
+        </>
+      )}
+    </footer>
+  );
+};
 
 export default Footer;
