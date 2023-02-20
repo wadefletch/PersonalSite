6a24df2218e4aaf2b2fba5dbb2d93cb2d21592b7
Wade Fletcher <wbfletch@gmail.com>
2023-02-20 14:47:14 -0500

    add commit hash to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 const Footer = () => (
   <footer className="text-sm text-gray-500 grid place-content-center max-w-2xl mx-auto p-5">
-    CC BY 4.0 &#x2022; 2023
+    CC BY 4.0 &#x2022; 2023 &#x2022;{' '}
+    {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.substring(0, 7)}
   </footer>
 );
 
