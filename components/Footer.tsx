633b4cc73be1ede1942d76ba4a6b115e3be2ea74
Wade Fletcher <wbfletch@gmail.com>
2023-02-20 10:21:51 -0500

    remove unimplemented sha hash

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => (
   <footer className=" text-gray-600 grid place-content-center max-w-2xl mx-auto p-10">
-    CC BY 4.0 &#x2022; 2023 &#x2022; 0b7bde4
+    CC BY 4.0 &#x2022; 2023
   </footer>
 );
 
