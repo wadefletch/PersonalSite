4475bc505083519c7a37f97bd4d1c4a6541f11b3
Wade Fletcher <wbfletch@gmail.com>
2023-02-20 15:00:20 -0500

    aligned footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,36 @@
+import { spawn } from 'child_process';
+
 const Footer = () => {
-  const sha = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.substring(0, 7);
+  const sha = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA;
+
+  const pieces = [
+    <a
+      key={0}
+      href="//creativecommons.org/licenses/by/4.0/"
+      className="no-underline"
+    >
+      CC BY 4.0
+    </a>,
+    <span key={1}>2023</span>,
+    sha && (
+      <a
+        key={2}
+        className="font-mono no-underline"
+        href={`https://github.com/wadefletch/wadefletcher.com/commit/${sha}`}
+      >
+        {sha.substring(0, 7)}
+      </a>
+    ),
+  ];
+  const dot = <span>&#x2022;</span>;
 
   return (
-    <footer className="text-sm text-gray-500 grid place-content-center max-w-2xl mx-auto p-5">
-      CC BY 4.0 &#x2022; 2023{' '}
-      {sha && (
+    <footer className="text-sm text-gray-500 flex gap-1 max-w-2xl mx-auto p-5 items-center justify-center">
+      {pieces.filter(Boolean).map((piece, i) => (
         <>
-          <span>&#x2022;</span> {sha}
+          {piece} {i < pieces.length - 1 && dot}
         </>
-      )}
+      ))}
     </footer>
   );
 };
