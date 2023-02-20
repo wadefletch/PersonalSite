a8ec4e5e5bb0f92315d29d7126438bc7f4bdb86b
Wade Fletcher <wbfletch@gmail.com>
2023-02-20 14:52:17 -0500

    fix hash spacing in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,11 +3,10 @@ const Footer = () => {
 
   return (
     <footer className="text-sm text-gray-500 grid place-content-center max-w-2xl mx-auto p-5">
-      CC BY 4.0 &#x2022; 2023
+      CC BY 4.0 &#x2022; 2023{' '}
       {sha && (
         <>
-          <span>&#x2022;</span>
-          {sha}
+          <span>&#x2022;</span> {sha}
         </>
       )}
     </footer>
