16114df1b8fa11e445889bf47f122fd52638dd04
Wade Fletcher <wbfletch@gmail.com>
2023-02-20 22:27:25 -0500

    fixed footer again

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,39 +1,36 @@
-import type { GetStaticProps } from 'next';
+const Footer = () => {
+  const commit = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA;
 
-interface FooterProps {
-  commit?: string;
-}
+  return (
+    <footer className="text-sm text-gray-500 flex gap-1 max-w-2xl mx-auto p-5 items-center justify-center">
+      <a href="//creativecommons.org/licenses/by/4.0/" className="no-underline">
+        CC BY 4.0
+      </a>
 
-const Footer = ({ commit }: FooterProps) => (
-  <footer className="text-sm text-gray-500 flex gap-1 max-w-2xl mx-auto p-5 items-center justify-center">
-    <a href="//creativecommons.org/licenses/by/4.0/" className="no-underline">
-      CC BY 4.0
-    </a>
+      <span>&#x2022;</span>
 
-    <span>&#x2022;</span>
+      <span>2023</span>
 
-    <span>2023</span>
+      <span>&#x2022;</span>
 
-    {commit ? (
-      <>
-        <span>&#x2022;</span>
+      {commit ? (
         <a
           className="font-mono no-underline"
           href={`https://github.com/wadefletch/wadefletcher.com/commit/${commit}`}
         >
           {commit.substring(0, 7)}
         </a>
-      </>
-    ) : null}
-  </footer>
-);
-
-export const getStaticProps: GetStaticProps = async () => {
-  return {
-    props: {
-      commit: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
-    },
-  };
+      ) : (
+        <a
+          href="//github.com/wadefletch/wadefletcher.com"
+          target="blank"
+          className="no-underline"
+        >
+          Source
+        </a>
+      )}
+    </footer>
+  );
 };
 
 export default Footer;
