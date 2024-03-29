5dbf1ed0de2f38c5e626887f5a73d8aa9cd136db
Wade Fletcher <3798059+wadefletch@users.noreply.github.com>
2024-03-29 16:28:23 -0400

    copy updates

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,19 +4,19 @@ export default function Hero() {
       <h1 className="text-4xl font-bold">Wade Fletcher</h1>
 
       <p>
-        I&apos;m an independent AI researcher and software engineer, working on
-        building scalable retrieval systems for LLMs.
+        I&apos;m a software engineer and AI hacker, working on building scalable
+        retrieval systems for LLMs.
       </p>
 
       <p>
-        Most recently, I was the founding engineer (and then CTO) at{' '}
+        Most recently, I was the first engineer at{' '}
         <a href="//atmos.ai" className="underline">
           Atmos AI
         </a>
-        , building TurboTax for ESG reporting. In 2024, Atmos was acquired by{' '}
-        <a href="//wapsustainability.com">a large ESG consulting firm</a>. I
-        also created <a href="//covintern.com">CovIntern</a>, a platform that
-        helped 120k+ students find remote internships during the COVID-19
+        , building TurboTax for ESG reporting. (In 2024, Atmos was acquired by{' '}
+        <a href="//wapsustainability.com">an ESG consulting firm</a>.) I also
+        created <a href="//covintern.com">CovIntern</a>, a platform that helped
+        120k+ students find high-quality remote internships during the COVID-19
         pandemic.
       </p>
 
